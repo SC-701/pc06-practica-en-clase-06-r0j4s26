fcab5420e44a883a49bd5ce5394a84607386ccfb
Sebastián Rojas Vargas <rojxsvargxs2607@gmail.com>
2026-03-01 15:05:10 -0600

    commit de la pc05 a pc06

---
+++ Vehiculo.API/BD/dbo/Stored Procedures/EliminarVehiculo.sql
@@ -0,0 +1,22 @@
+﻿-- =============================================
+-- Author:		Sebastián Rojas Vargas
+-- Create date: 1/19/26
+-- Description:	Eliminar Vehiculo
+-- =============================================
+CREATE PROCEDURE EliminarVehiculo
+	-- Add the parameters for the stored procedure here
+	@Id uniqueidentifier
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	BEGIN TRANSACTION
+		DELETE 
+		FROM     Vehiculo
+		WHERE  (Id = @Id)
+		SELECT @Id
+	COMMIT TRANSACTION
+END
